5050ceea5b4f6ba9cd214c3c3c1e3465e315dbe2
alexey0511 <om.alex@hotmail.com>
2022-08-17 11:52:13 +1200

    FEAT: add player to canvas
    * add player
    * add simple logic for bouncing

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,32 @@
+// NOTE:
+// script is Work in Progress
+// TODO: 
+/**
+ * NOTE: script is work in progress
+ * 
+ * TODO: 
+ *  - tidy up layout
+ *  - import types rather than hardcode
+ *  - import or declare socket.io script
+ *  - avoid using any
+ */
+type Coords = {
+  x: number;
+  y: number;
+};
+
+type Player = {
+  id: string;
+  name: string;
+  color: string;
+  coords: Coords;
+};
+
+type State = {
+  puck: Coords;
+  players: Player[];
+};
+
 // namespace, to keep funcs together
 const App: any = {
   config: {
@@ -12,14 +41,11 @@ const App: any = {
 };
 
 // define client actions
-App.setPlayer = function (player: any) {
+App.setPlayer = function (player: Player) {
   App.player = player;
 };
 
-App.drawPlayer = function (
-  coords: { x: number; y: number },
-  color: string | CanvasGradient | CanvasPattern
-) {
+App.drawPlayer = function (coords: Coords, color: string) {
   const canvas = document.getElementById(
     App.config.canvasId
   ) as HTMLCanvasElement;
@@ -36,47 +62,63 @@ App.drawPlayer = function (
   }
 };
 
-App.redrawFromState = function (state: { puck: any }) {
+App.redrawFromState = function (state: State) {
   App.clear();
 
   App.drawPuck(state.puck);
   // TODO: draw players on positions
-  // state.players.forEach(player => {
-  //   App.drawPlayer(player.coords, player.color);
-  // })
+  state.players.forEach((player) => {
+    App.drawPlayer(player.coords, player.color);
+  });
 };
 
-App.drawPuck = function (coords: { x: any; y: any }) {
-  const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
-  const context = canvas.getContext("2d");
-  const centerX = coords.x;
-  const centerY = coords.y;
-  const radius = 20;
+App.drawPuck = function ({ x, y }: Coords) {
+  const canvas = document.getElementById(
+    App.config.canvasId
+  ) as HTMLCanvasElement;
+  if (canvas) {
+    const context = canvas.getContext("2d");
 
-  if (context) {
-    context.beginPath();
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = "green";
-    context.fill();
-    context.closePath();
+    if (context) {
+      context.beginPath();
+      context.arc(x, y, App.config.radius, 0, 2 * Math.PI, false);
+      context.fillStyle = "red";
+      context.fill();
+      context.closePath();
+    }
   }
 };
 
 App.clear = function () {
   const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
+
+  if (!canvas) return;
   const context = canvas.getContext("2d");
-  if (context) {
-    context.clearRect(
-      0,
-      0,
-      App.config.canvasSize.width,
-      App.config.canvasSize.height
-    );
-  }
+
+  if (!context) return;
+  context.clearRect(
+    0,
+    0,
+    App.config.canvasSize.width,
+    App.config.canvasSize.height
+  );
 };
 
+document
+  .getElementById("game-canvas")!
+  .addEventListener("mousemove", function (e: MouseEvent) {
+    App.socket.emit("playerMove", {
+      id: App.socket.id,
+      position: {
+        x: e.pageX - this.offsetLeft,
+        y: e.pageY - this.offsetTop,
+      },
+    });
+  });
+
 // SOCKET COMMUNICATION
-declare var io: any; // TEMP:
+// TODO: declare global io module or maybe just include as part of script
+// @ts-expect-error need to register module
 App.socket = io.connect("http://localhost:8000"); // NOTE: hardcoded PORT (needs to change)
 App.socket.on("stateUpdate", App.redrawFromState);
 App.socket.on("setPlayer", App.setPlayer);
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,9 @@ export type Coords = {
 };
 
 export type Player = {
+  id: string;
   name: string;
+  color: string;
   coords: Coords;
 };
 
@@ -25,7 +27,13 @@ const config = {
   CANVAS_WIDTH: 800,
   CANVAS_HEIGHT: 400,
   RADIUS: 20,
+  REFRESH_RATE: 16, // ~60fps...
 };
+
+const INITIAL_SPEED = 1;
+let speedSlowing = 0.01;
+let speedX = INITIAL_SPEED;
+let speedY = INITIAL_SPEED;
 
 const { CANVAS_HEIGHT, CANVAS_WIDTH, RADIUS } = config;
 const server = app.listen(PORT, function () {
@@ -39,35 +47,66 @@ app.get("/", function (req, res) {
 });
 
 // THE APP
+
 let state: State = {
   puck: {
-    x: 25,
-    y: 25,
+    x: CANVAS_WIDTH / 2, // start in the middle
+    y: CANVAS_HEIGHT / 2,
   },
   players: [],
 };
 
-let moveX = 1;
-let moveY = 1;
+function updatePuckPosition(puck: Coords, players: Player[]) {
+  if (speedX !== 0) {
+    speedX = speedX > 0 ? speedX - speedSlowing : speedX + speedSlowing;
+  }
 
-function updatePuckPosition(
-  puck: Coords,
-  field: { height: number; width: number },
-  players: Player[]
-) {
+  if (speedY !== 0) {
+    speedY = speedY > 0 ? speedY - speedSlowing : speedY + speedSlowing;
+  }
+
   // detect collision
-  if (puck.x > CANVAS_WIDTH - RADIUS || puck.x < RADIUS) moveX = -moveX;
-  if (puck.y > CANVAS_HEIGHT - RADIUS || puck.y < RADIUS) moveY = -moveY;
+  // walls
+  // TODO: might need more complicated reflection
+  if (puck.x > CANVAS_WIDTH - RADIUS || puck.x < RADIUS) speedX = -speedX;
+  if (puck.y > CANVAS_HEIGHT - RADIUS || puck.y < RADIUS) speedY = -speedY;
+
+  // players
+  players.forEach((p) => {
+    if (
+      Math.abs(puck.x - p.coords.x) < 2 * RADIUS &&
+      Math.abs(puck.y - p.coords.y) < 2 * RADIUS
+    ) {
+      // restart speed
+      // TODO: might need to base it on force applied
+
+      // reset speed
+      speedX = INITIAL_SPEED;
+      speedY = INITIAL_SPEED;
+
+      // bounce (reflect) the ball
+      if (puck.x < p.coords.x) {
+        speedX = -speedX;
+      }
+
+      if (puck.y < p.coords.y) {
+        speedY = -speedY;
+      }
+    }
+  });
 
   return {
-    x: puck.x + moveX,
-    y: puck.y + moveY,
+    x: puck.x + speedX,
+    y: puck.y + speedY,
   };
 }
-function makePlayer(playersNum: number) {
+
+function makePlayer(playerId: string, playersNum: number) {
   return {
+    id: playerId,
     name: `Player ${playersNum}`,
-    coords: { x: 0, y: 0 },
+    color: "green",
+    coords: { x: playersNum * 10, y: playersNum * 20 },
   };
 }
 
@@ -75,20 +114,30 @@ function makePlayer(playersNum: number) {
 const io = new Server(server);
 
 io.sockets.on("connection", function (socket) {
-  const player = makePlayer(state.players.length);
+  const player = makePlayer(socket.id, state.players.length);
   state.players.push(player);
 
-  socket.emit("setPlayer", { name: player.name });
+  socket.emit("setPlayer", {
+    name: player.name,
+  });
 
   setInterval(() => {
     state = {
       ...state,
-      puck: updatePuckPosition(
-        state.puck,
-        { height: config.CANVAS_HEIGHT, width: config.CANVAS_WIDTH },
-        state.players
-      ),
+      puck: updatePuckPosition(state.puck, state.players),
     };
     socket.emit("stateUpdate", state);
-  }, 100);
+  }, config.REFRESH_RATE);
+
+  socket.on("playerMove", (data: any) => {
+    state.players.forEach((player) => {
+      if (player.id === data.id) {
+        player.coords = data.position;
+      }
+    });
+  });
+
+  socket.on("disconnect", (reason) => {
+    state.players = state.players.filter((p) => p.id !== socket.id);
+  });
 });
